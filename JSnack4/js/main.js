f5aec20c32ad36d76181fddfa1d0c9f23a06af92
DanieleMarino70 <danielaotitan@gmail.com>
2023-01-29 18:23:04 +0100

    add: jsnack4

--- JSnack4/js/main.js
+++ JSnack4/js/main.js
@@ -1,3 +1,43 @@
 // JSnack4 (Bonus)
 // Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
+
+const firstListNumbers = [];
+const secondListNumbers = [];
+const indexFirstListNumber = Math.floor(Math.random() * 10 + 1); // da 1 a 10
+const indexSecondListNumber = Math.floor(Math.random() * 10 + 1);
+for (i = 0; i < indexFirstListNumber; i++) {
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+  firstListNumbers[i] = randomNumber;
+}
+
+for (i = 0; i < indexSecondListNumber; i++) {
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+  secondListNumbers[i] = randomNumber;
+}
+
+console.log("La prima lista è composta da: ", firstListNumbers);
+console.log("-------------------");
+console.log("La seconda lista è composta da: ", secondListNumbers);
+
+
+
+if (firstListNumbers.length > secondListNumbers.length) {
+  for (let i = secondListNumbers.length; i < firstListNumbers.length; i++) {
+    const randomNumber = Math.floor(Math.random() * 100 + 1);
+    secondListNumbers[i] = randomNumber;
+  }
+
+ } else {
+  for (let i = firstListNumbers.length; i < secondListNumbers.length; i++) {
+    const randomNumber = Math.floor(Math.random() * 100 + 1);  
+    firstListNumbers[i] = randomNumber;
+  }
+}
+
+
+
+
+console.log(firstListNumbers);
+console.log("------------");
+console.log(secondListNumbers);
